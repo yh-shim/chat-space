6cc9c86b7b60981d83c431eda4ce40af36d3fc23
yh-shim <yh.shimada@gmail.com>
2019-08-20 17:26:24 +0900

    Model-route/controller(action)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'chats#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :chats, only: [:index, :create]
+  end
 end
