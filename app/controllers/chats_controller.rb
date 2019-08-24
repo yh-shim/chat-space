16f826f858e3d0358b1a5a0e0f745b4a38b17bfc
yh-shim <yh.shimada@gmail.com>
2019-08-24 23:10:48 +0900

    add Asynchronous to messageFunc

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -10,7 +10,10 @@ class ChatsController < ApplicationController
   def create
     @chat = @group.chats.new(chat_params)
     if @chat.save
-      redirect_to group_chats_path(@group), notice: 'メッセージが送信されました'
+      respond_to do |format|
+        format.html { redirect_to group_chats_path(@group), notice: 'メッセージが送信されました' }
+        format.json
+      end
     else
       @chats = @group.chats.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
